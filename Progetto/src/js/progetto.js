f33ed00cb477f643f3324c70680b5da4b068d8a2
AngelaDiPietro <angeladipietro92@gmail.com>
2018-10-24 18:34:50 +0200

    restyling with bootstrap

--- Progetto/src/js/progetto.js
+++ Progetto/src/js/progetto.js
@@ -13,7 +13,7 @@ var like=document.getElementsByClassName('like');
 for (var i = 0; i <like.length;  i++) {
 	like[i].addEventListener("click",function(){
 		
-		if (this.style.backgroundColor="white"){
+		if (this.style.backgroundColor==="white"){
 			this.style.backgroundColor="#32cd32";
 			}else{
 			this.style.backgroundColor="white";	
